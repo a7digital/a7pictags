b1f9598dc1d4feb79bb62f3852310fa50260b07e
Matteo Bonaker <bonaker@a7digital.de>
2020-03-24 10:13:32 +0100

    Add bare bone structure of extension

---
+++ ext_emconf.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @noinspection PhpUndefinedVariableInspection */
+$EM_CONF[$_EXTKEY] = array(
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '9.5.0-',
+        ),
+    ),
+);
